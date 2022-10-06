d19946a1fa6e425978598d12f9d13a093a3bce1b
Calvaria <Zhiyao20021123@gmail.com>
2022-10-06 13:38:33 +0800

    test

---
+++ insoLyrics.Tests/Test1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace insoLyrics.Tests.Lyrics.FromNetease
+{
+    [TestClass]
+    public class Test1
+    {
+        private static Process _process;
+        [TestMethod]
+        public void testProccess()
+        {
+            _process = Process.GetProcessesByName("osu!").FirstOrDefault();
+            Console.WriteLine(_process);
+            return;
+            if (Registry.GetValue(@"HKEY_CLASSES_ROOT\osu!\shell\open\command", null, null) is string exec) {
+
+                Console.WriteLine(exec.Split('"')[1]);
+                _process = Process.Start(exec.Split('"')[1]);
+                Console.WriteLine(_process);
+            }
+
+        }
+    } 
+}
